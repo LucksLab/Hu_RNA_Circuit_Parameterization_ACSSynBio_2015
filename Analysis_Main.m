bd3da2488dc213dcb1aee55f488568332e5b19ea
jblucks <jblucks@cornell.edu>
2015-06-07 16:08:16 -0400

    Added citation information for Hu et al. 'Generating effective models and parameters for RNA genetic circuits' ACS SynBio, 2015, http://dx.doi.org/10.1021/acssynbio.5b00077 .

--- Analysis_Main.m
+++ Analysis_Main.m
@@ -3,12 +3,18 @@
 % School of Chemical and Biomolecular Engineering,
 % Cornell University, Ithaca NY 14853 USA.
 %
+% Details about this software can be found in 
+% Hu et al. “Generating effective models and parameters for RNA genetic circuits” 
+% ACS SynBio, 2015, http://dx.doi.org/10.1021/acssynbio.5b00077 .
+%
+%
 % Permission is hereby granted, free of charge, to any person obtaining a copy
 % of this software and associated documentation files (the "Software"), to deal
 % in the Software without restriction, including without limitation the rights
 % to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 % copies of the Software, and to permit persons to whom the Software is
-% furnished to do so, subject to the following conditions: % The above copyright notice and this permission notice shall be included in
+% furnished to do so, subject to the following conditions: 
+% The above copyright notice and this permission notice shall be included in
 % all copies or substantial portions of the Software.
 %
 % THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
